4c6c41fb797b92659abb82ff6ab7bbdeab206b50
System Administrator <root@Guilhermes-MacBook-Pro.local>
2018-05-12 19:28:52 -0300

    Teste salario testador menor que 2000 passando

--- src/main/java/Funcionario.java
+++ src/main/java/Funcionario.java
@@ -9,28 +9,28 @@
  * @author guimat
  */
 class Funcionario {
-
+    
     String nome;
     String email;
     double salarioBase;
     String cargo;
-
+    
     void setNome(String pNome) {
         this.nome = pNome;
     }
-
+    
     void setEmail(String pEmail) {
         this.email = pEmail;
     }
-
+    
     void setSalarioBase(double pSalarioBase) {
         this.salarioBase = pSalarioBase;
     }
-
+    
     void setCargo(String pCargo) {
         this.cargo = pCargo;
     }
-
+    
     double getSalarioLiquido() {
         if (this.cargo.equals("DESENVOLVEDOR")) {
             if (this.salarioBase >= 3000) {
@@ -38,18 +38,22 @@ class Funcionario {
             } else {
                 return this.salarioBase * 0.9;
             }
-        } else if (this.cargo.equals("DBA")) {
+        } else if (this.cargo.equals("DBA")){
             if (this.salarioBase >= 2000) {
                 return this.salarioBase * 0.75;
             } else {
                 return this.salarioBase * 0.85;
             }
-        } else if (this.cargo.equals("TESTADOR")) {
-            return this.salarioBase * 0.75;
+        } else if (this.cargo.equals("TESTADOR")){
+            if (this.salarioBase >= 2000) {
+                return this.salarioBase * 0.75;
+            } else {
+                return this.salarioBase * 0.85;
+            }
         } else {
             return 0;
         }
-
+        
     }
-
+    
 }
